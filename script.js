040e6881112102e550462820396936666ce7b857
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-04-03 17:50:51 -0400

    solved

--- script.js
+++ script.js
@@ -31,3 +31,14 @@ function solution(str) {
 }
 
 solution("world");
+
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+function greet(name, owner) {
+  // Add code here
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
