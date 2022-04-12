5110e5464e71396835d46eac2a57a50adf6ef6c7
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-04-12 13:03:18 -0400

    js

--- script.js
+++ script.js
@@ -93,3 +93,13 @@ function lovefunc(flower1, flower2) {
     return false;
   }
 }
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+  //...
+  if (bool === true) {
+    return "Yes";
+  } else if (bool === false) {
+    return "No";
+  }
+}
