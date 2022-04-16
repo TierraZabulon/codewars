53115fb2a27e577531ea18a199c391e3a6be34f8
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-04-15 21:16:13 -0400

    js

--- script.js
+++ script.js
@@ -118,3 +118,9 @@ function stringToArray(string) {
   const strings = string.split(" ");
   return strings;
 }
+
+//Complete the function to return his total number of goals in all three leagues.
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
