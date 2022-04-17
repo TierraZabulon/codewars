7ba18df74a2234a92bf1aa550458e7fe54ab687c
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-04-17 16:10:27 -0400

    js

--- script.js
+++ script.js
@@ -124,3 +124,9 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+//Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
