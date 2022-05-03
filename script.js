364da4dc9cd418ad8d06ccb385453c44b0bff3e8
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-05-02 21:58:49 -0400

    js

--- script.js
+++ script.js
@@ -167,3 +167,7 @@ function rentalCarCost(d) {
   }
 }
 //8kyu continued//green
+//We need a simple function that determines if a plural is needed or not.
+function plural(n) {
+  return n !== 1;
+}
