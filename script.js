f2b6ddcec6f90a7e78b0175000b5aeaefc10fd8e
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-05-09 12:37:55 -0400

    js

--- script.js
+++ script.js
@@ -251,3 +251,12 @@ function getEvenNumbers(numbersArray) {
     return number % 2 === 0;
   });
 }
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+function solution(str, ending) {
+  // TODO: complete
+  if (str.endsWith(ending)) {
+    return true;
+  } else {
+    return false;
+  }
+}
