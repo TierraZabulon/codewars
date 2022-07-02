6d6052c2a40c67ae1bbf626d89cdb9000760e863
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-07-02 11:35:24 -0400

    codew

---
+++ python/drinkabout.py
@@ -0,0 +1,17 @@
+
+# Rules:
+
+# Children under 14 old.
+# Teens under 18 old.
+# Young under 21 old.
+# Adults have 21 or more.
+
+def people_with_age_drink(age):
+    if age < 14:
+        return "drink toddy"
+    elif age < 18:
+        return "drink coke"
+    elif age < 21:
+        return "drink beer"
+    elif age >= 21:
+        return "drink whisky"
