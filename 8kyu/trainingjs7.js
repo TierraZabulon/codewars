99a97da5ba8b4d936dd11672cb50b83bc271b479
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-08-22 11:35:32 -0400

    cw

--- 8kyu/trainingjs7.js
+++ 8kyu/trainingjs7.js
@@ -13,3 +13,16 @@ function saleHotdogs(n) {
 //Return: test should return price of hotdogs based on the number of customers
 //Example: n < 5 should return 100 per hotdog
 //Psudocode: if there is only one customer in line ordering the total would come out to 100
+
+//try as ternery
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n >= 10 ? n * 90 : null;
+}
+
+//template
+//   var foo = (
+//   bar === 'a' ? 1 : // if
+//   bar === 'b' ? 2 : // else if
+//   bar === 'c' ? 3 : // else if
+//   null // else
+// );
