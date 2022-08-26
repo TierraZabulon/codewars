3f66da0b7ff96e9499194b9e2131c80d00aa7e2f
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-08-26 08:24:54 -0400

    cw

--- 8kyu/invertValues.js
+++ 8kyu/invertValues.js
@@ -4,3 +4,15 @@
 // invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 // invert([]) == []
 // You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+  const myFunction = (num) => {
+    if (num < 0) {
+      return Math.abs(num);
+    } else if ((num) => 0) {
+      return -num;
+    }
+  };
+  let newArr = array.map(myFunction);
+  return newArr;
+}
