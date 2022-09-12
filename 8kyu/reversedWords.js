8ecff0a6e6d35c4e2271ed5ec2cebf152372076d
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-09-11 21:00:23 -0400

    cw

--- 8kyu/reversedWords.js
+++ 8kyu/reversedWords.js
@@ -1,3 +1,9 @@
 // Complete the solution so that it reverses all of the words within the string passed in.
 
 // Example(Input --> Output):
+function reverseWords(str) {
+  let str1 = str.split(" ");
+  let strR = str1.reverse();
+  let strJ = strR.join(" ");
+  return strJ;
+}
