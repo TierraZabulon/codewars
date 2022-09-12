1025d82a6f4f828bd3d19309be3c12a2b7442442
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-09-12 19:37:14 -0400

    cw

--- 8kyu/doIGetABonus.js
+++ 8kyu/doIGetABonus.js
@@ -3,3 +3,14 @@
 // Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
 
 // If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+function bonusTime(salary, bonus) {
+  let s = salary * 10;
+  s = s.toString();
+  let n = salary.toString();
+
+  if (bonus === true) {
+    return "\u00A3" + s;
+  } else {
+    return "\u00A3" + n;
+  }
+}
